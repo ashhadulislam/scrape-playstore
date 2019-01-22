cbfab41e4d8d4ad7ea88d294d27b61ca84cdddad
ashhadulislam@gmail.com <ashhadulislam@gmail.com>
2019-01-22 21:24:25 +0530

    image

--- app_reviews/scripts/scripts.py
+++ app_reviews/scripts/scripts.py
@@ -291,7 +291,7 @@ def NLP_Flow(df,column_name,stop_words):
     plt.imshow(wordcloud, interpolation='bilinear')
     plt.axis("off")
     print("current directory is ",os.getcwd())
-    print(contstants.media_location, os.path.exists(contstants.media_location))
+    print(constants.media_location, " ", os.path.exists(constants.media_location))
     print("going to save at ",constants.media_location)
 
     plt.savefig(constants.media_location+"wordcloud.png")
