3bacbcb1a1b8933f3aa9e2052f82968ad0ae28c4
ashhadulislam@gmail.com <ashhadulislam@gmail.com>
2019-03-13 16:34:37 +0530

    add

--- app_reviews/scripts/constants.py
+++ app_reviews/scripts/constants.py
@@ -10,14 +10,14 @@ print("current directory is ",current_dir)
 env="heroku_uat"
 
 if env=="heroku_uat":
-	# chrome_driver_location='/app/.chromedriver/bin/chromedriver'
-	chrome_driver_location=os.path.join(current_dir,".chromedriver","bin","chromedriver")
-	# google_chrome_bin="/app/.apt/usr/bin/google-chrome"
-	google_chrome_bin=os.path.join(current_dir,".apt","usr","bin","google-chrome")
-	# media_location='/app/media/app_review_tools/'
-	# output_location='/app/app_reviews/outputs/'
-	media_location=os.path.join(current_dir,"media","app_review_tools")
-	output_location=os.path.join(current_dir,"app_reviews","outputs")
+	chrome_driver_location='/app/.chromedriver/bin/chromedriver'
+	# chrome_driver_location=os.path.join(current_dir,".chromedriver","bin","chromedriver")
+	google_chrome_bin="/app/.apt/usr/bin/google-chrome"
+	# google_chrome_bin=os.path.join(current_dir,".apt","usr","bin","google-chrome")
+	media_location='/app/media/app_review_tools/'
+	output_location='/app/app_reviews/outputs/'
+	# media_location=os.path.join(current_dir,"media","app_review_tools")
+	# output_location=os.path.join(current_dir,"app_reviews","outputs")
 
 elif env=="dev":
 	# chrome_driver_location='app_reviews/software/chromedriver'
